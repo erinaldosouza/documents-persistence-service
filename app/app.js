4d4c467493722fab01f06bc483604474181e67e5
Francisco Erinaldo de Souza <t_francisco@hapvida.com.br>
2019-04-16 13:29:26 -0300

    changed from gridfs stream to gridfs bucket

--- app/app.js
+++ app/app.js
@@ -4,23 +4,29 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const multer  = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
 const bodyParser = require('body-parser');
-const ObjectId = require('mongodb').ObjectID;
 const passport = require('passport');
-const HeaderAPIKeyStrategy = require('passport-headerapikey').HeaderAPIKeyStrategy
+const ObjectID = require('mongodb').ObjectID;
+const HeaderAPIKeyStrategy = require('passport-headerapikey').HeaderAPIKeyStrategy;
+const { GridFSBucket } = require('mongodb')
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 
+const port = 5000;
+
+app.listen(port, ()=> {
+    console.log(`Server started on port ${port}`)
+})
+
 // Config api key
 const securityHeaderConfig = { header: 'api-key', prefix: 'Api-Key-' };
 const API_KEY = 'a';
     
 // Verifying api key
-passport.use( new HeaderAPIKeyStrategy(securityHeaderConfig, false, (apikey, done) => {
+passport.use(new HeaderAPIKeyStrategy(securityHeaderConfig, false, (apikey, done) => {
         return done(null, API_KEY === apikey)
     }
 ));
@@ -40,8 +46,7 @@ mongoose.connect(mongoURI, mongoOptions, (err, conn) => {
     
     if(err) console.log(err);
     
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('user_docs');
+    gfs = new GridFSBucket(conn.db, {bucketName: 'user_docs'});
 
 });
 
@@ -73,33 +78,31 @@ const upload = multer({ storage });
 // Read all files from MongoDB
 // Saying that the url / must have the api key
 app.get('/', passport.authenticate('headerapikey', passportSession), (req, res) => {
-    gfs.files.find().toArray((err, files) => {  
+    gfs.find().toArray((err, files) => {  
         if(!files || files.lengh === 0) {
             return res.status(404).json({
                 err: 'No file found'
             });
         }
-
         return res.json(files);
     })
 });
 
 // Read a specific file from MongoDB
 app.get('/:id', passport.authenticate('headerapikey', passportSession), (req, res) => {
-    gfs.files.findOne({_id: new ObjectId(req.params.id)}, (err, file) => {
-        if(!file || file.lengh === 0) {
+    gfs.find(new ObjectID(req.params.id)).next((err, files) => {  
+        if(!files || files.lengh === 0) {
             return res.status(404).json({
                 err: 'No file found'
             });
         }
-
-        return res.json(file);
+        return res.json(files);
     })
 });
 
 // display a single file from MongoDB
 app.get('/img/:id', passport.authenticate('headerapikey', passportSession), (req, res) => {
-    gfs.files.findOne({_id: new ObjectId(req.params.id)}, (err, file) => {  
+    gfs.find(new ObjectID(req.params.id)).next((err, file) => {  
         if(!file || file.lengh === 0) {
             return res.status(404).json({
                 err: 'No file found'
@@ -107,7 +110,7 @@ app.get('/img/:id', passport.authenticate('headerapikey', passportSession), (req
         }
 
         if(file.contentType === "image/jpeg" || file.contentType === "image/png") {
-            const readStream = gfs.createReadStream(file.filename);
+            const readStream = gfs.openDownloadStream(file._id);
             readStream.pipe(res);
         } else {
             resizeTo.status(404).json({
@@ -122,16 +125,11 @@ app.post('/', upload.single('file'), (req, res) => {
 });
 
 app.delete('/:id', passport.authenticate('headerapikey', passportSession), (req, res) => {
-    gfs.remove({_id: req.params.id, root: 'user_docs'}, (err, gridStore) => {
+    console.log("ID", req.params.id)
+    gfs.delete(new ObjectID(req.params.id), (err, gridStore) => {
          if(err) {
-             return err.status(500).json({err: err});
+             return res.status(500).json({err: err});
          }
-
          return res.status(200).json({msg: 'success'})
     })
-})
-const port = 5000;
-
-app.listen(port, ()=> {
-    console.log(`Server started on port ${port}`)
-})
+});
